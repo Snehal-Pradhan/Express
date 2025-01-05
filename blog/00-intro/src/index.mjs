9b98c31d0377485bab8878c63e913a37d7394ba5
Snehal-Pradhan <snehalpradhan23@gmail.comnode>
2025-01-05 20:30:40 +0530

    route parameters

--- blog/00-intro/src/index.mjs
+++ blog/00-intro/src/index.mjs
@@ -1,9 +1,7 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-const PORT  = process.env.PORT || 3000;
-
-
+const PORT = process.env.PORT || 3000;
 
 // what is a route?
 // why showed /cannot get
@@ -13,22 +11,38 @@ const PORT  = process.env.PORT || 3000;
 // app.{method}("{route}",{requestHandler})
 // request handler is just a callback funtion with two arguments one request and another one is response. -> both of them are objects
 // requstHandler = (req,res)=>{
-//    res.send("hello")    
+//    res.send("hello")
 //    }
 
-app.get('/',(req,res)=>{
-    res.status(201).send("hello world");;
-})
+const mockUser = [
+  { id: 1, username: "Deanne Livingstone", lastName: "Deanne" },
+  { id: 2, username: "Erena Steckings", lastName: "Erena" },
+  { id: 3, username: "Brunhilde Scothorne", lastName: "Brunhilde" },
+  { id: 4, username: "Sibella Catterick", lastName: "Sibella" },
+  { id: 5, username: "Adora Grabeham", lastName: "Adora" },
+];
 
-app.get("/api/users",(req,res)=>{
-    res.send([{"id":1,"username":"Deanne Livingstone","lastName":"Deanne"},
-        {"id":2,"username":"Erena Steckings","lastName":"Erena"},
-        {"id":3,"username":"Brunhilde Scothorne","lastName":"Brunhilde"},
-        {"id":4,"username":"Sibella Catterick","lastName":"Sibella"},
-        {"id":5,"username":"Adora Grabeham","lastName":"Adora"}]);
-})
+app.get("/", (req, res) => {
+  res.status(201).send("hello world");
+});
 
+app.get("/api/users", (req, res) => {
+  res.send(mockUser);
+});
 
-app.listen(PORT,()=>{
-    console.log(`server runnig on Port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server runnig on Port ${PORT}`);
+});
+
+// route parameters
+// what if i need a specific data which satisfies that parameter.
+app.get("/api/users/:id", (req, res) => {
+  const parsedId = parseInt(req.params.id);
+  if (isNaN(parsedId))
+    return res.status(400).send({ msg: "Bad Request. Invalid ID" });
+  const findUser = mockUser.find((user) => user.id === parsedId);
+  if (!findUser) {
+    return res.sendStatus(404);
+  }
+  res.send(findUser);
+});
