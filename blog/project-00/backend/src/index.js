b5b3c7292d09df6ea26a694249a7354d7bc676fa
Snehal-Pradhan <snehalpradhan23@gmail.comnode>
2025-01-06 16:36:07 +0530

    server working great

--- blog/project-00/backend/src/index.js
+++ blog/project-00/backend/src/index.js
@@ -1,14 +1,35 @@
 const express = require("express");
-
+const users = require("./data/MOCK_DATA.json");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const host = "localhost"; // Add quotes around 'localhost'
+const host = "localhost";
 
 app.get("/", (req, res) => {
   res.send("home route");
 });
 app.get("/about", (req, res) => {
   res.send("about");
+});
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+app.get("/api/users/:id", (req, res) => {
+  if (!/^\d+$/.test(req.params.id)) {
+    return res.status(400).send("Bad Request: Invalid ID");
+  }
+
+  const parsedid = parseInt(req.params.id, 10);
+
+  if (isNaN(parsedid)) {
+    return res.status(400).send("Bad Request: Invalid ID");
+  }
+  const findid = users.find((user) => user.id === parsedid);
+
+  if (!findid) {
+    return res.status(404).send("User not found");
+  }
+
+  res.status(200).json(findid);
 });
 
 app.listen(PORT, () => {
