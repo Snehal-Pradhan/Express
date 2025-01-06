24417ec5173c9e91e8050b2a559c0f5a7ed41cc7
Snehal-Pradhan <snehalpradhan23@gmail.comnode>
2025-01-06 16:04:42 +0530

    added some routes

--- blog/project-00/backend/src/index.js
+++ blog/project-00/backend/src/index.js
@@ -4,6 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const host = "localhost"; // Add quotes around 'localhost'
 
+app.get("/", (req, res) => {
+  res.send("home route");
+});
+app.get("/about", (req, res) => {
+  res.send("about");
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://${host}:${PORT}`);
 });
